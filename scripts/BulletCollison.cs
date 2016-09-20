41429024404cb459f78f632fc70602f89f430595
Nate Wang <ytowang@ucdavis.edu>
2016-09-20 11:07:02 -0700

    Add scripts

---
+++ scripts/BulletCollison.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+ * destroy bullet itself on hit
+ * damage hit target if it is tagged "Destroyable"
+ * bullet has to be trigger
+ * attach this to bullets 
+ */
+public class BulletCollison : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Destroyable"))
+        {
+            Debug.Log(other.gameObject.name);
+            Destroy(gameObject);
+            Destroy(other.gameObject);//TODO: modify to damage()
+        }
+    }
+}
---
+++ scripts/CharaShoot.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+ * shoot bullets on click (firerate limited)
+ * bullet direction: player -> mouseposition(touchposition)
+ * attach this to player
+ */
+public class CharaShoot : MonoBehaviour {
+    public Rigidbody2D bullet;
+    public float bulletSpeed = 2f;
+    Vector2 v;
+    private float fireRate = 0.5f;
+    private float lastShot = 0.0f;
+    void Start () {
+	
+	}
+
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))//TODO: change to touch method
+        {
+            Fire();
+        }
+    }
+    public void Fire()
+    {
+        if (Time.time > fireRate + lastShot) 
+        {
+            Vector2 target = Input.mousePosition;
+            v = (Vector2)Camera.main.WorldToScreenPoint(transform.position);
+            Rigidbody2D b = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody2D;
+            b.velocity = (target - v).normalized * bulletSpeed;
+            lastShot = Time.time;
+        }
+
+    }
+}
